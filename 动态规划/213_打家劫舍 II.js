48ab261597c624e91aa6db203c10854938d8d2ed
lhy_innno <haoyuli40@gmail.com>
2022-08-30 10:55:38 +0800

    feat: dp

--- 动态规划/213_打家劫舍 II.js
+++ 动态规划/213_打家劫舍 II.js
@@ -16,3 +16,20 @@ var rob = function(nums) {
   }
   return Math.max(sum(0, nums.length-2), sum(1, nums.length-1))
 };
+
+
+var rob = function(nums) {
+  if (nums.length === 1) return nums[0]
+  let scan = (le, ri) => {
+      let dp = []
+      dp[le] = nums[le]
+      dp[1+le] = Math.max(nums[le], nums[1+le])
+      for (let i = le+2; i<ri; i++) {
+          dp[i] = Math.max(dp[i-1], dp[i-2]+nums[i])
+      }
+      return dp[ri-1]
+  }
+  let a = scan(0, nums.length-1)
+  let b = scan(1, nums.length)
+  return Math.max(a, b)
+};
--- 动态规划/337_打家劫舍 III.js
+++ 动态规划/337_打家劫舍 III.js
@@ -17,3 +17,16 @@ var rob = function(root) {
   dp.set(root, Math.max(steal, no))
   return dp.get(root)
 };
+
+// 超时代码
+var rob = function(root) {
+  if (!root) return 0
+  let val1 = 0
+  val1+=root.val
+  if (root.left) val1 = val1 + rob(root.left.left) + rob(root.left.right)
+  if (root.right) val1 = val1 + rob(root.right.right) + rob(root.right.left)
+  let val2 = 0
+  val2+=rob(root.left)
+  val2+=rob(root.right)
+  return Math.max(val2, val1)
+};
