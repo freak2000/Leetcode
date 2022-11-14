f982566bc754c77c6ee44af0a3e17b92233556ad
lhy <haoyuli40@gmail.com>
2022-11-14 17:49:34 +0800

    feat: 模拟

---
+++ 模拟/58_最后一个单词的长度.js
@@ -0,0 +1,10 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+  s = s.trim()
+  s = s.replace(/\s+/g, ' ')
+  let arr = s.split(' ')
+  return arr[arr.length-1].length
+};
